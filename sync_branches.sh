41e8a046c20a72e34b64511e308a76ca4f005b51
Sandyana Rae Williams <sandyana.r.w@outloook.com>
2025-04-22 19:49:23 +0800

    add Synchronizing_branches script

---
+++ sync_branches.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+remote_branches=$(git branch -r | grep -vE "HEAD|main|master" | sed 's/origin\///')
+
+for branch in $remote_branches; do
+  echo "--------------------------Synchronizing branches: $branch ----------------------------"
+  
+  if git show-ref --quiet refs/heads/"$branch"; then
+    git checkout "$branch"
+    git pull origin "$branch"
+  else
+    git checkout -b "$branch" origin/"$branch"
+  fi
+done
+
+git checkout -
